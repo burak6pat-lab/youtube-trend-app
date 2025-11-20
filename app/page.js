302a5ff72f17092072ac56ac32ae4b434c9aa997
burak6pat-lab <burak6pat@gmail.com>
2025-11-20 10:37:26 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="bg-black text-white min-h-screen p-10">
+      <h1 className="text-4xl font-bold mb-6">
+        YouTube Trend Analyzer
+      </h1>
+
+      <p className="text-gray-400">
+        Soldaki filtreleri kullanarak son 7–365 gün arası hızlı yükselen videoları listeleyebilirsiniz.
+      </p>
+    </main>
+  );
+}
